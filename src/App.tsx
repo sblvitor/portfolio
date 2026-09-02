ea8d4d64e596c35968f3c76b77ef3ae08be38adc
Vitor Lira <sblvitor@gmail.com>
2026-09-01 20:11:52 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Canvas } from "@react-three/fiber"
+import { Experience } from "./experience"
+
+function App() {
+
+  return (
+    <>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 6]
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ src/experience.tsx
@@ -0,0 +1,14 @@
+import { OrbitControls } from "@react-three/drei"
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+
+      <mesh>
+        <boxGeometry />
+      </mesh>
+
+    </>
+  )
+}
