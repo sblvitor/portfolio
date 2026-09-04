e65220667b6c965a38b3688433fc1d754f43c133
Vitor Lira <sblvitor@gmail.com>
2026-09-03 23:31:40 -0400

    up shadow opacity

--- src/experience.tsx
+++ src/experience.tsx
@@ -6,7 +6,7 @@ import { useControls } from "leva"
 export const Experience = () => {
 
   const { opacity, positionY, blur, scale } = useControls('contactShadow', {
-    opacity: { value: 0.2, min: 0, max: 1 },
+    opacity: { value: 0.25, min: 0, max: 1 },
     positionY: { value: -1.5, min: -3, max: 0 },
     blur: { value: 1, min: 0, max: 3 },
     scale: { value: 7, min: 0, max: 20 },
