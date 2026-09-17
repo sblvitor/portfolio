3ed601b20cd09c00581dd240c554cb3468f5152f
Vitor Lira <sblvitor@gmail.com>
2026-09-16 21:04:20 -0400

    add the iframe to screen

--- src/laptop-model.tsx
+++ src/laptop-model.tsx
@@ -1,4 +1,4 @@
-import { useGLTF } from "@react-three/drei"
+import { Html, useGLTF } from "@react-three/drei"
 
 useGLTF.preload('https://threejs-journey.com/resources/models/macbook_model.gltf')
 
@@ -7,6 +7,16 @@ export const LaptopModel = (props: any) => {
   const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
 
   return (
-    <primitive object={computer.scene}  {...props} />
+    <primitive object={computer.scene}  {...props}>
+      <Html
+        transform
+        wrapperClass="htmlScreen"
+        distanceFactor={1.17}
+        position={[0, 1.56, -1.4]}
+        rotation-x={-0.256}
+      >
+        <iframe src="https://wave-grid-rho.vercel.app/" />
+      </Html>
+    </primitive>
   )
 }
